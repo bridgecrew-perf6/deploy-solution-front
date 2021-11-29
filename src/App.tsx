f6600c880fd19f72a53b654ef844f6c5aaa0988c
Yohann <ydulong@adyc.fr>
2021-11-29 16:05:30 +0100

    [init]

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,41 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {MessageDto} from "./dto/message.dto";
 
 function App() {
+
+  const [backMessage, setBackMessage] =  useState<string>('');
+
+  async function getMessage() : Promise<void> {
+
+    fetch( `http://localhost:3000/message`, {
+    method: "GET",
+    headers: {'Content-Type': 'application/json'},
+    }).then( async (value: Response) => {
+      const response : MessageDto = await value.json();
+      setBackMessage(response.message);
+
+    }).catch((er) => {
+      alert(er);
+    });
+
+  }
+
+  useEffect(() => {
+    getMessage();
+
+  }, []);
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="logo-helyx.png" className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Message from back : {backMessage}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
---
+++ src/dto/message.dto.ts
@@ -0,0 +1,4 @@
+export class  MessageDto {
+    id: number = -1;
+    message : string = '';
+}
